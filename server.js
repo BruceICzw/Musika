31ad44731c4a5fa4ccc064d3c6a42ba438fb4490
BruceICzw <h220396z@hit.ac.zw>
2024-03-22 09:04:58 +0200

    Set Up Database Connection

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const connectDB = require('./database/db');
 
 const app = express();
 
 
 const PORT = 3000;
-
+connectDB();
 //Starting the server
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server runin on port ${PORT}....`);
 })
