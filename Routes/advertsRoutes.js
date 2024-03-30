0e5ea106b1c418a00ed7856d51c123f6ab47db6e
BruceICzw <h220396z@hit.ac.zw>
2024-03-30 12:20:47 +0200

    Add Adverts Contoller and Routes

---
+++ Routes/advertsRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const advertsController = require('../Controllers/advertsController');
+const Auth = require('../Middleware/auth')
+const router = express.Router();
+
+//Create new advert
+router.post('/upload', Auth.userAuth, advertsController.createAdvert)
+
+
+//Get all approved adverts
+router.get('/', advertsController.getAllApprovedAdverts)
+
+//Get approved adverts by category
+router.get('/:category', advertsController.getAdvertsByCategory)
+
+module.exports = router
