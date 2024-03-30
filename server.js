d13cb43affeed7f26bc9073950ea7e356c7309b2
BruceICzw <h220396z@hit.ac.zw>
2024-03-30 12:26:07 +0200

    Refactor code

--- server.js
+++ server.js
@@ -1,12 +1,43 @@
 const express = require('express');
-const connectDB = require('./database/db');
-
+const connectDB = require('./database/db')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const path = require('path')
+const { adminAuth, userAuth } = require('./Middleware/auth.js')
 const app = express();
 
-
 const PORT = 3000;
+//Middlewares
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", require('./Auth/Route'))
+app.use('/api/adverts', express.static(path.join(__dirname, 'public/images')), require('./Routes/advertsRoutes.js'));
+app.use('/api/admin', require('./Routes/adminRoutes.js'))
+app.use('/api/user/', require('./Routes/userRoutes.js'))
+app.use(cors())
+
+//Connecting the database
 connectDB();
+
+// ************** //
+
+app.set("view engine", "ejs")
+app.get("/", (req, res) => res.render("home"))
+app.get("/signup", (req, res) => res.render("register"))
+app.get("/login", (req, res) => res.render("login"))
+app.get('/admin', adminAuth, (req, res) => res.render("admin"));
+app.get('/admin/pending', adminAuth, (req, res) => res.render('pending'))
+app.get('/products', (req, res) => res.render("products"));
+app.get('/adverts/upload', userAuth, (req, res) => res.render('upload'))
+app.get('/user/profile', userAuth, (req, res) => res.render('profile'))
+
 //Starting the server
-app.listen(PORT, () => {
-    console.log(`Server runin on port ${PORT}....`);
+const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+//Error handler for every unhandledRejection error
+process.on('unhandledRejection', err => {
+    console.log(`An error occurred: ${err.message}`);
+    server.close(() => process.exit(1))
 })
